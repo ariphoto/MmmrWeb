cdd1fb4949d8bc05207b09bb24f513d771433a32
yk <15110010@oic-ok.ac.jp>
2017-11-21 14:02:14 +0900

    テーブルに新規行を追加するJSを記述 重複してるCSSを排除

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,5 +6,33 @@ function insertRow() {
 
     btn_insert.classList.add('disabled'); // 新規追加ボタンを無効化する
 
-    var rows = list_table.insertRow(-1);
+    var row_num = list_table.rows[0].cells.length; // テーブルの項目数を取得
+    var rows = list_table.insertRow(1); // 新規行を追加する
+
+    // テーブルにセル追加
+    for(var i = 0;i < row_num - 1;i++) {
+
+        var td;
+
+        switch (i){
+            // 名前 テキストボックス
+            case 1:
+                td = rows.insertCell(-1); // セルを追加する
+                var box = document.createElement('input');
+                td.appendChild(box);
+                break;
+            // 追加 ボタン
+            case row_num - 2:
+                td = rows.insertCell(-1); // セルを追加する
+                td.setAttribute('colspan','2'); // セル結合
+                var btn = document.createElement('button');
+                btn.classList.add('btn','btn-info','btn-original');
+                var txt = document.createTextNode("追加");
+                btn.appendChild(txt);
+                td.appendChild(btn);
+                break;
+            default:
+                td = rows.insertCell(-1); // セルを追加する
+        }
+    }
 }
