36c4aebcef7dfb053e654b438f6d9fbcf6bc6d12
yk <15110010@oic-ok.ac.jp>
2017-12-14 09:48:17 +0900

    園児編集(編集中)2

---
+++ models/school.js
@@ -0,0 +1,35 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+const school = loader.database.define('school', {
+    schoolId: {
+        type: Sequelize.STRING(128),
+        primaryKey: true,
+        validate: {
+            len: [8,20]
+        }
+    },
+    mailAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [8,20]
+        }
+    },
+    salt: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},{charset: 'utf8mb4',collate: 'utf8mb4_general_ci'});
+module.exports = school;
